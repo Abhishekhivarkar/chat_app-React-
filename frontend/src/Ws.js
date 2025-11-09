79dc626dcc0b417c27a7e631b61f2b8379ea9149
Abhishekhivarkar <Abhihivarkar783@gmail.com>
2025-11-09 12:35:30 +0530

    Fix

--- frontend/src/Ws.js
+++ frontend/src/Ws.js
@@ -1,6 +1,10 @@
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 
-function clientServer(){
- return io("http://localhost:3000")
+function clientServer() {
+  return io("https://chat-app-react-zrys.onrender.com", {
+    transports: ["websocket"],  // force WebSocket only
+    withCredentials: true,      // allow cross-domain cookies if needed
+  })
 }
+
 export default clientServer
